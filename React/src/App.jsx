cef075cf95e87efaf98089a6e9847c780d34dff0
Prince sharma <connect.princesharma@gmail.com>
2024-10-15 22:58:13 +0530

    React_Project_setup

---
+++ React/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from './components/Nav'
+const App = () => {
+  return (
+    <>
+    <Nav></Nav>
+    </>
+  )
+}
+
+export default App
---
+++ React/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+    <div className="container"><p>Logo</p></div>
+    <div className="nav">
+        <a href="#">Home</a>
+    </div>
+    </>
+  )
+}
+
+export default Nav
