414517fffb46c7175251b75dc097ec1a3e9b534a
lavish <lavishkamboj42@gmail.com>
2024-10-24 11:48:07 +0530

    first container in body added

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
+import Body from "./components/body";
 
 const App = () => {
   return (
     <>
       <Header></Header>
       <Nav></Nav>
+      <Body></Body>
     </>
   );
 };
